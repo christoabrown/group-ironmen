521eb0558b72f02ff15ef5461fec16b4fb849f5e
Wouter Rutgers <git@wouterrutgers.nl>
2024-10-06 19:45:57 +0200

    cache dumps

--- cache/prepare.sh
+++
@@ -1,8 +0,0 @@
-echo "Copying cache files"
-
-echo "Downloading xteas.json"
-id=$(curl -s https://archive.openrs2.org/caches.json | jq -r '[.[] | select(.game=="oldschool" and .environment=="live" and .language=="en" and (.sources | contains(["Jagex"]))) | .id] | max')
-
-curl -o cache/xteas.json https://archive.openrs2.org/caches/runescape/$id/keys.json
-curl -o cache/disk.zip https://archive.openrs2.org/caches/runescape/$id/disk.zip
-unzip cache/disk.zip -d cache
